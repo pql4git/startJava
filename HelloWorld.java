5c141ba71b58ee7303b734ab5bcb3f3ae24c66e7
hou9625 <peng4icloud@gmail.com>
2025-09-14 16:44:47 +0900

    初回変更、日付表示など基本的な部分から順次追加

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,20 @@
-   public class HelloWorld {  
+import java.time.LocalDate;       // 日付 (例: 2025-09-14)
+import java.time.LocalTime;       // 時刻 (例: 10:30:45)
+import java.time.LocalDateTime;   // 日付＋時刻
+import java.time.ZonedDateTime;   // タイムゾーン付き日付時刻
+import java.time.format.DateTimeFormatter; // 日付時刻のフォーマット
+import java.time.Duration;        // 時間の差分（時間・分・秒）
+import java.time.Period;          // 日付の差分（年・月・日）
+import java.util.Date;
+public class HelloWorld {  
        public static void main(String[] args) {  
            System.out.println("Hello, World!"); 
+           System.out.println("2回目の練習");
+           Date today4print = new Date();
+           System.out.println( today4print); 
+           LocalDateTime now = LocalDateTime.now();
+           System.out.println(now);
+           DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+           System.out.println(now.format(fmt));
        }
    }
